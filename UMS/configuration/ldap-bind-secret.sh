4d0d3354c94d8a1a8d5c45a445b41e15e235882f
sachinkj <sachinkj@in.ibm.com>
2020-05-07 14:37:51 +0530

    UMS ldap configuration

---
+++ UMS/configuration/ldap-bind-secret.sh
@@ -0,0 +1,2 @@
+oc create secret generic ldap-bind-secret \
+    --from-literal=ldapUsername="cn=root" --from-literal=ldapPassword="passw0rd" --from-literal=lc_ldap_bind_dn="cn=root"  --from-literal=lc_ldap_bind_password="passw0rd" 
