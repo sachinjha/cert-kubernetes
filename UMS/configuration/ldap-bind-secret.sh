d94fabe0f86ebc954d4d9442ab7a24c1dcad2adf
sachinkj <sachinkj@in.ibm.com>
2020-05-13 17:26:42 +0530

    ext_ldap changes

--- UMS/configuration/ldap-bind-secret.sh
+++ UMS/configuration/ldap-bind-secret.sh
@@ -1,2 +1,8 @@
 oc create secret generic ldap-bind-secret \
-    --from-literal=ldapUsername="cn=root" --from-literal=ldapPassword="passw0rd" --from-literal=lc_ldap_bind_dn="cn=root"  --from-literal=lc_ldap_bind_password="passw0rd" 
+    --from-literal=ldapUsername="cn=root" \
+    --from-literal=ldapPassword="passw0rd" \
+    --from-literal=lc_ldap_bind_dn="cn=root"  \
+    --from-literal=lc_ldap_bind_password="passw0rd" \
+    --from-literal=externalLdapUsername="cn=root"  \
+    --from-literal=externalLdapPassword="passw0rd"  
+    
