2628a545d8cac4050fb853341977b076d1e72661
sachinkj <sachinkj@in.ibm.com>
2020-05-14 12:40:16 +0530

    ban secret creation script

--- BAN/configuration/ban-secret.sh
+++ BAN/configuration/ban-secret.sh
@@ -2,9 +2,11 @@
 kubectl create secret generic ibm-ban-secret \
   --from-literal=navigatorDBUsername="db2inst1" \
   --from-literal=navigatorDBPassword="passw0rd" \
-  --from-literal=ldapUsername="cn=admin,dc=GSI-Labs-Cognizant,dc=cloud" \
+  --from-literal=ldapUsername="cn=root" \
 --from-literal=ldapPassword="passw0rd" \
+--from-literal=externalLdapUsername="cn=root" 
+--from-literal=externalLdapPassword="passw0rd" \
   --from-literal=keystorePassword="passw0rd" \
   --from-literal=ltpaPassword="passw0rd" \
---from-literal=appLoginUsername="icnadmin" \
+--from-literal=appLoginUsername="ceadmin" \
 --from-literal=appLoginPassword="passw0rd"
