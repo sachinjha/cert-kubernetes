b70621882299a51cbfe6af52c7441ad65bc858a4
sachinkj <sachinkj@in.ibm.com>
2020-11-13 13:29:42 +0530

    reset passwords

--- BAN/configuration/ban-secret.sh
+++ BAN/configuration/ban-secret.sh
@@ -1,8 +1,8 @@
 kubectl create secret generic ibm-ban-secret \
   --from-literal=navigatorDBUsername="db2inst1" \
-  --from-literal=navigatorDBPassword="lti@db2passw0rd" \
+  --from-literal=navigatorDBPassword="db2passw0rd" \
    --from-literal=ldapUsername="cn=root" \
---from-literal=ldapPassword="LTI@TDIPassw0rd" \
+--from-literal=ldapPassword="TDIPassw0rd" \
   --from-literal=keystorePassword="passw0rd" \
   --from-literal=ltpaPassword="passw0rd" \
   --from-literal=appLoginUsername="ceadmin" \
--- UMS/configuration/ldap-bind-secret.sh
+++ UMS/configuration/ldap-bind-secret.sh
@@ -1,8 +1,8 @@
 oc create secret generic ldap-bind-secret \
     --from-literal=ldapUsername="cn=root" \
-    --from-literal=ldapPassword="LTI@TDIPassw0rd" \
+    --from-literal=ldapPassword="TDIPassw0rd" \
     --from-literal=lc_ldap_bind_dn="cn=root"  \
-    --from-literal=lc_ldap_bind_password="LTI@TDIPassw0rd" \
+    --from-literal=lc_ldap_bind_password="TDIPassw0rd" \
     --from-literal=externalLdapUsername="cn=root"  \
-    --from-literal=externalLdapPassword="LTI@TDIPassw0rd"  
+    --from-literal=externalLdapPassword="TDIPassw0rd"  
     
